3714ae1b8aec807ce3f373674394d6e7fb47742f
Christopher Chen <christopher.chen1995@gmail.com>
2017-09-20 13:46:44 -0700

    Comments

--- Exercise 8/main.cpp
+++ Exercise 8/main.cpp
@@ -2,12 +2,16 @@
 
 using namespace std;
 int main(){
+	// initialize variables
 	int i;
 	char c;
+
 	cout << "Enter a number between 65 and 90: ";
 	cin >> i;
+	// cast the user-entered int to a char (look up ASCII table)
 	c = static_cast<char>(i);
 	cout << c << endl;
+	// print out an offset from the user entered char
 	cout << static_cast<char>(c+32) << endl;
 	return 0;
 	
