53d060527159382455abc4b735304ddea35495ac
Christopher Chen <christopher.chen1995@gmail.com>
2017-10-20 13:28:16 -0700

    Make 18 easier

--- Exercise 18/main.cpp
+++ Exercise 18/main.cpp
@@ -4,25 +4,22 @@
 using namespace std;
 int main() {
 	int n = 1;
-	while (n > 0) {
 
-		cout << "Enter a positive integer or zero to stop: ";
-		cin >> n;
-		// loop over height of figure
-		for (int i = n; i >= 1; i--)
+	cin >> n;
+	// loop over height of figure
+	for (int i = n; i >= 1; i--)
+	{
+		// print out the whitespace on the left of each line
+		for (int j = 0; j < n - i; j ++)
 		{
-			// print out the whitespace on the left of each line
-			for (int j = 0; j < n - i; j ++)
-			{
-				cout << "  ";
-			}
-			// print out the asterisks on the right
-			for (int j = 0; j < i; j ++)
-			{
-				cout << "* ";
-			}
-			cout << endl;
+			cout << "  ";
 		}
+		// print out the asterisks on the right
+		for (int j = 0; j < i; j ++)
+		{
+			cout << "* ";
+		}
+		cout << endl;
 	}
 	return 0;
 
