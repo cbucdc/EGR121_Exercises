08a4036479b02f829b8a3a65f6bda2053f9cab72
Christopher Chen <christopher.chen1995@gmail.com>
2017-10-20 13:30:39 -0700

    Comments

--- Exercise 21/main.cpp
+++ Exercise 21/main.cpp
@@ -1,10 +1,11 @@
 // Inspired by exercise Set 19.docx
 /*
-A student has been given a grayscale image as a 2D array of floating point numbers.
+A researcher has been given a grayscale image as a 2D array of floating point numbers.
 
 In a grayscale image each pixel value represents the brightness of the pixel.
 
 The image is encoded as a C++ array here:
+```
 double image[8][8] = {{0.7, 0.8, 1.0, 0.5, 0.5, 0.0, 1.0, 0.5},
 	{0.7, 0.5, 1.0, 0.5, 0.6, 1.0, 0.8, 0.6},
 	{0.5, 0.1, 1.0, 0.1, 0.1, 1.0, 0.3, 0.7},
@@ -14,12 +15,15 @@ double image[8][8] = {{0.7, 0.8, 1.0, 0.5, 0.5, 0.0, 1.0, 0.5},
 	{0.9, 1.0, 1.0, 1.0, 1.0, 1.0, 0.2, 1.0},
 	{0.9, 0.0, 1.0, 0.1, 0.3, 0.3, 0.6, 0.7}
 };
+```
+This researcher wishes to reduce this data to a simpler measurement, such as the average value of the pixels.
 
-What is the average pixel brightness in this 2d grayscale image?
+The Problem:
+Write a program that takes this array and print the average pixel brightness of this 2d grayscale image.
 
 Print out that value with two decimal places of precision.
 
-Have fun finding out what's in the image :)
+It's a bit distorted, but have fun finding out what's in the image :)
 
 */
 #include <iostream>
